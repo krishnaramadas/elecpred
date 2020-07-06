f6a2c47d7cfe73521aec29d7ee8a6c5d78bcea30
Krishna Ramadas <38761386+krishnaramadas@users.noreply.github.com>
2020-07-06 16:10:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
-app = Flask(__name__) #Initialize the flask App
+elecpredapp = Flask(__name__) #Initialize the flask App
 model = pickle.load(open('model.pkl', 'rb'))
 
-@app.route('/')
+@elecpredapp.route('/')
 def home():
     return render_template('index.html')
   
 
-@app.route('/predict',methods=['POST'])
+@elecpredapp.route('/predict',methods=['POST'])
 def predict():
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
